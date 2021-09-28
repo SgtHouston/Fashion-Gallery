c4b7bf6588737468a246c1211a9e2f6211e67b22
Chris Houston <chrisbcoding247@gmail.com>
2021-09-28 03:05:53 -0400

    initialized and set up basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+
+          {/* Page 1  */}
+          <Route exact path="/">
+            SLR Couture Future Web Page 
+          </Route>
+
+          {/* Page 2  */}
+          <Route exact path="/about">
+            
+          </Route>
+
+          {/* Page 3  */}
+          <Route exact path="/gallery">
+            
+          </Route>
+
+          {/* Page 3:  */}
+          <Route exact path="/gallery/:galleryId">
+            
+          </Route>
+
+          {/* Redirect */}
+          <Route exact path="*">
+            <Redirect to="/">{/* Redirect to homepage like so */}</Redirect>
+          </Route>
+
+
+          
+
+
+        </Switch>
+      </Router>
     </div>
   );
 }
