a5213af766d0424c9a45f23efeaad5457849515a
Chris Houston <chrisbcoding247@gmail.com>
2021-10-23 22:08:21 -0400

    added home gallery images and text

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
+
 
 function Contact() {
     return (
         <div>
             < Header/>
             Contact Us Page Coming Soon...
+            < Footer/>
         </div>
     )
 }
--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
+
 
 function Gallery() {
     return (
         <div>
             < Header />
             Gallery Page Coming Soon...
+            
+            < Footer/>
         </div>
     )
 }
