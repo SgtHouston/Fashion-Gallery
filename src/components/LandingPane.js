389a5ef689cbb238300bc6e1d8e115e101785d6b
Chris Houston <chrisbcoding247@gmail.com>
2021-10-27 02:32:00 -0400

    added pics and changed main pics

--- src/components/LandingPane.js
+++ src/components/LandingPane.js
@@ -7,12 +7,16 @@ import CustomerWorn3 from '../images/IMG_6994.png'
 import CustomerWorn4 from '../images/Twins.jpg'
 import CustomerWorn5 from '../images/IMG_6979.png'
 import CustomerWorn6 from '../images/SilverProm2.JPG'
-import CustomerWorn7 from '../images/IMG_2463.jpg'
-import CustomerWorn8 from '../images/GoldFull.JPG'
-import CustomerWorn9 from '../images/Couple1.JPG'
+import CustomerWorn7 from '../images/IMG-9180.jpg'
+import CustomerWorn8 from '../images/Pageant.jpg'
+import CustomerWorn9 from '../images/Couple1.png'
 import CustomerWorn10 from '../images/Couple2.JPG'
-import CustomerWorn11 from '../images/Couple3.JPG'
-import CustomerWorn12 from '../images/PinkFlower.JPG'
+import CustomerWorn11 from '../images/PurpleBlueDress.png'
+import CustomerWorn12 from '../images/IMG_9619.png'
+import CustomerWorn13 from '../images/PinkPose.png'
+import CustomerWorn14 from '../images/PinkFlower.JPG'
+import CustomerWorn15 from '../images/SilverPose.png'
+import CustomerWorn16 from '../images/GoldTrain.png'
 
 
 
@@ -25,10 +29,9 @@ function LandingPane() {
             <video className='videoTag' autoPlay loop muted>
                 <source className='video' src={backgroundVid} type='video/mp4' />
             </video>
-            <div className="text-center">
-                
-                <span className="text-center landing_text"> Exquisite Bespoke Designs </span>
 
+            <div className="text-center">
+                <span className="text-center landing_text"> Exquisite Bespoke Designs </span>
             </div>
             <div className="dressdisplay">
                 <div className="landing__img">
@@ -41,16 +44,14 @@ function LandingPane() {
                     <img className='img-fluid' src={CustomerWorn3} alt="Customer Worn Dress 3"></img>
                 </div>
             </div>
-            <div className="text-center">
-                
-                <span className="text-center landing_text"> Look And Feel Your Best With SLR Couture </span>
 
+            <div className="text-center">
+                <span className="text-center landing_text"> Look And Feel Your Best With SLR Couture </span>
             </div>
             <img className='img-fluid large-img' src={CustomerWorn4} alt="Customer Worn Dress 4" ></img>
+            
             <div className="text-center">
-                
                 <span className="text-center landing_text"> A Dress For The Occasion Of A Lifetime </span>
-                
             </div>
             <div className="dressdisplay">
                 <div className="landing__img">
@@ -63,16 +64,14 @@ function LandingPane() {
                     <img className='img-fluid' src={CustomerWorn7} alt="Customer Worn Dress 7" ></img>
                 </div>
             </div>
-            <div className="text-center">
-                
-                <span className="text-center landing_text"> Custom Formalwear Fitted Just For You </span>
 
+            <div className="text-center">
+                <span className="text-center landing_text"> Custom Formalwear Fitted Just For You </span>
             </div>
             <img className='img-fluid large-img' src={CustomerWorn8} alt="Customer Worn Dress 8" ></img>
+            
             <div className="text-center">
-                
                 <span className="text-center landing_text"> Collaborate With Us To Create A Work Of Art </span>
-
             </div>
             <div className="dressdisplay">
                 <div className="landing__img">
@@ -85,12 +84,31 @@ function LandingPane() {
                     <img className='img-fluid' src={CustomerWorn11} alt="Customer Worn Dress 11" ></img>
                 </div>
             </div>
-            <div className="text-center">
-                
-                <span className="text-center landing_text"> Make Your Special Occasion One To Remember </span>
 
+            <div className="text-center">
+                <span className="text-center landing_text"> Make Your Special Occasion One To Remember </span>
             </div>
             <img className='img-fluid large-img-last' src={CustomerWorn12} alt="Customer Worn Dress 12" ></img>
+            
+            <div className="text-center">
+                <span className="text-center landing_text"> Another Creative Line Here </span>
+            </div>
+            <div className="dressdisplay">
+                <div className="landing__img">
+                    <img className="img-fluid" src={CustomerWorn13} alt="Customer Worn Dress 13" ></img>
+                </div>
+                <div className="landing__img">
+                    <img className='img-fluid' src={CustomerWorn14} alt="Customer Worn Dress 14" ></img>
+                </div>
+                <div className="landing__img">
+                    <img className='img-fluid' src={CustomerWorn15} alt="Customer Worn Dress 15" ></img>
+                </div>
+            </div>
+
+            <div className="text-center">
+                <span className="text-center landing_text"> Another Creative Line Here </span>
+            </div>
+            <img className='img-fluid large-img-last' src={CustomerWorn16} alt="Customer Worn Dress 16" ></img>
         </div>
     )
 }
