558a3f5a3cb6fe78ddf02977fdf2d5847628eb21
Chris Houston <chrisbcoding247@gmail.com>
2022-02-12 17:26:10 -0500

    set up db for faqs and lookbook

--- src/db/FaqsDB.js
+++ src/db/FaqsDB.js
@@ -32,10 +32,6 @@ const FaqsDB = [
         Answer: 'Yes, we do extend our couture services to international brides. Consultations will be held virtually via a video call, during which we will walk you through the entire couture process in detail.'
     },
     {
-        Question: 'HOW SOON CAN I BEGIN THE COUTURE PROCESS FOR MY GOWN?',
-        Answer: 'We advise beginning the process at least 24 weeks to your wedding. We do offer express couture services. Please send an e-mail to slrcouture@gmail.com if you need a gown less than 24 weeks to your wedding.'
-    },
-    {
         Question: 'HOW LONG DOES SHIPPING TAKE?',
         Answer: "Shipping usually takes between 5-7 business days for domestic shipping within the U.S.A. Express shipping is an option as well. \n International shipping usually takes anywhere from five business days to 10 business days, depending on the country. \n At SLR Couture, we work hard to make sure every package is delivered on time and in excellent condition. However, once your order leaves our showroom we have very little control over UPS or Canada Post and we are unable to make any changes to the package. We do not ship to a P.O Box. All orders are shipped via UPS Ground or Canada Post. Expedited shipping is available for an additional charge. \n We will send an email once your order has shipped, including a tracking number, so you can follow the package's journey until it arrives at your doorstep. Swatches do not have tracking at this time. \n \n For Atlanta Area Customers: Pick-ups are available for dresses at our showroom location or you can choose to have them shipped. Packages are signature required. We are not responsible for packages that are lost or stolen by the delivery carrier. Insurance options are available per request. \n"
     },
