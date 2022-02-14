c551738da416fb34875f1d2ab78457379ce2c427
Chris Houston <chrisbcoding247@gmail.com>
2022-02-14 15:19:43 -0500

    added border radius to pic popups for client and landing

--- src/components/ImagePopupClient.js
+++ src/components/ImagePopupClient.js
@@ -30,7 +30,7 @@ function ImagePopupClient({ Img }) {
                         {/* one style for mobile/one for desktop */}
                         <dialog className="dialog center" open style={{ width: '30rem' }} onClick={handleClick} >
                             <h5 className="dialog-message"><CloseIcon className="dialog-icon"/> </h5>
-                            <img className='img-fluid client-image' src={Img} style={{ maxHeight: '90vh' }} onClick={handleClick} alt="" />
+                            <img className='img-fluid client-image-client' src={Img} style={{ maxHeight: '90vh' }} onClick={handleClick} alt="" />
                         </dialog>
                     </div>
                 ) 
