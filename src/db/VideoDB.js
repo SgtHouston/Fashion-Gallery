ff95c34410f9d1e9971160ea0e9a33271c2fd168
Chris Houston <chrisbcoding247@gmail.com>
2022-08-01 15:25:32 -0400

    reordered videodb

--- src/db/VideoDB.js
+++ src/db/VideoDB.js
@@ -3,8 +3,8 @@ import video2 from '../video/WhiteGoldCouple.mp4'
 import video3 from '../video/WhiteGold.mp4'
 import video4 from '../video/BlueDress.mp4'
 import video5 from '../video/WeddingApplique.mp4'
-import video6 from '../video/Green.mp4'
-import video7 from '../video/WeddingAppliqueBack2.mp4'
+import video6 from '../video/WeddingAppliqueBack2.mp4'
+import video7 from '../video/Green.mp4'
 
 
 const VideoDB = [
