6f7a76ec8ea763010348f4b0d5d5c736e5d62ad7
Chris Houston <cmhoust@amazon.com>
2025-11-22 14:36:11 -0800

    feat: coordinated React 18 + Material-UI upgrade
    - Upgrade React 17 → 18.3.1 and React DOM
    - Update Material-UI packages to v7+ (React 18 compatible)
    - Update react-redux to v8 (React 18 compatible)
    - Migrate to createRoot API for React 18
    - Use legacy-peer-deps to handle transition conflicts
    
    Resolves circular dependency issues by upgrading all related packages together.
    App tested and working locally.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store'
 import { Provider } from 'react-redux'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     {/* Import store from redux to provide to app globally */}
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
